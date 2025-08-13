00142849959903a70ed7cafbbf8f9b07c476855a
Brad Holland <hollanbm@gmail.com>
2025-08-12 21:02:43 -0400

    toset

--- pfsense.tf
+++ pfsense.tf
@@ -1,5 +1,5 @@
 resource "pfsense_dnsresolver_hostoverride" "dns_entry" {
-  for_each = var.nginxproxymanager.subdomains
+  for_each = toset(var.nginxproxymanager.subdomains)
 
   host         = each.key
   domain       = var.nginxproxymanager.domain_name
