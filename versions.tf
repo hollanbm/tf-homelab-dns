226e0111256f1b57dbb901ef4f7158923bdc5a60
Brad Holland <hollanbm@gmail.com>
2026-06-02 23:47:34 -0400

    remove nginx proxymanager requirement

--- versions.tf
+++ versions.tf
@@ -1,10 +1,6 @@
 terraform {
-  required_version = "~>1.9"
+  required_version = "~>1.12"
   required_providers {
-    nginxproxymanager = {
-      source  = "Sander0542/nginxproxymanager"
-      version = ">= 1, < 2"
-    }
     pfsense = {
       source  = "marshallford/pfsense"
       version = ">= 0, < 1"
